766ce1d8cba9db6e2afbf36f89eba18293e55da9
Bryce Kerley <bkerley@brycekerley.net>
2015-12-06 14:03:22 -0500

    move database

--- include/config.h
+++ include/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define YOSPOS 219
-#define DBNAME "test.sqlite3"
+#define DBNAME "data/puzzlebox.sqlite3"
 
 #define VICTIM_USERNAME "Migishu"
 #define VICTIM_PASSWORD "Grimey Drawer"
